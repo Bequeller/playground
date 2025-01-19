35efca0884d633274121cb661fcd40a48d03d433
Bequeller <bqueller@gmail.com>
2025-01-20 01:00:07 +0900

    delete README_CD.md

--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 This is a simple Python script that prints "Hello, World!" to the console.
 """
 
-"""
-! This is an important comment
-? This is a question
-// This is a normal comment
-todo: This is a TODO comment
-* This is a highlighted comment
-normal comment
-"""
+# ! This is an important comment
+# ? This is a question
+# // This is a normal comment
+# todo: This is a TODO comment
+# * This is a highlighted comment
+# normal comment
 
 
 def main():
