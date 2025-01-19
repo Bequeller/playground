21345420443a13c5405bf6e04fdfb8929627999f
Bequeller <bqueller@gmail.com>
2025-01-19 21:56:54 +0900

    git actions added: pytest

--- src/tdd_test.py
+++ src/tdd_test.py
@@ -1,3 +1,8 @@
-
+'''
+Test 코드 작성
+'''
 def hello_world():
+    '''
+    Hewllo World!!를 반환하는 함수
+    '''
     return "Hello World!!"
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,6 +1,11 @@
-
+'''
+pytest 테스트 코드
+'''
 from src.tdd_test import hello_world
 
 
 def test_hello_world():
+    '''
+    hello_world 함수를 테스트하는 함수
+    '''
     assert hello_world() == "Hello World!!"
